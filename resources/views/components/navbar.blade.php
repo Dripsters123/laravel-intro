9b65e896ba91c0e0d3356830a5602feb999de52a
eihents <eihents@vtdt.edu.lvgit>
2024-04-24 13:43:41 +0300

    nesapratu ko izdarijam

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,8 @@
+<header>
+    <nav>
+        <x-nav-link href="/" value>Home</x-nav-link>
+        <x-nav-link href="/about" value>About</x-nav-link>
+        <x-nav-link href="/contact" value>Contact</x-nav-link>
+
+    </nav>
+</header>
